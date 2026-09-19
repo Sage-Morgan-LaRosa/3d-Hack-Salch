8a855d376c2514ae4a6fa1dc8f6cdcf52949d6e2
Sage Morgan-LaRosa <samo4421@colorado.edu>
2026-09-18 23:07:32 -0600

    spawns and mob movment
    added spawn and movment for mobs and add grave stones

--- 3d-hack&slash/player.gd
+++ 3d-hack&slash/player.gd
@@ -14,13 +14,6 @@ var _camera_input_direction := Vector2.ZERO
 @onready var _camera: Camera3D = %Camera3D
 
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_click"):
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	if event.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
-
 func _unhandled_input(event: InputEvent) -> void:
 	var is_camera_motion := (
 		event is InputEventMouseMotion and
